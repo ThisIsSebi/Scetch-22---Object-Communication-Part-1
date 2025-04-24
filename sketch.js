b0ba4e23fca907afccdfbc341da8bf68e4f6fc62
ThisIsSebi <shoff439@gmail.com>
2025-04-24 08:10:04 +0200

    changed code

--- sketch.js
+++ sketch.js
@@ -1,7 +1,43 @@
+let bubbles = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 400);
 }
 
 function draw() {
-  background(220);
+  background(0);
+}
+
+class Bubble {
+  constructor(x, y, r) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.brightness = 0;
+  }
+
+  changeColor(bright){
+this.brightness = bright;
+  }
+
+  contains(px, py){
+    let d = dist(px, py, this.x, this.y);
+    if(d < this.r){
+      return true;
+    } else{
+      return false;
+    }
+  }
+  
+  move() {
+    this.x = this.x + random(-2, 2);
+    this.y = this.y + random(-2, 2);
+  }
+
+  show() {
+    stroke(255);
+    strokeWeight(4);
+    fill(this.brightness, 125);
+    ellipse(this.x, this.y, this.r * 2);
+  }
 }
