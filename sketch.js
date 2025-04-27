6c63c3e751f2defe1d091e08c9d7ca9207c24607
ThisIsSebi <shoff439@gmail.com>
2025-04-27 09:29:49 +0200

    Two bubbles intersect and change colour

--- sketch.js
+++ sketch.js
@@ -1,47 +1,61 @@
+
+
+
 let bubble1;
 let bubble2;
-
 
 function setup() {
   createCanvas(600, 400);
   bubble1 = new Bubble(200, 200);
-  bubble2 = new Bubble(300,200);
+  bubble2 = new Bubble(300, 200);
 }
 
 function draw() {
   background(0);
- 
-  let d = dist(bubble1.x, bubble1.y, bubble2.x, bubble2.y);
-  if(d < bubble1.r + bubble2.r){
-    background(200,0,100);
+
+  if (bubble1.intersects(bubble2)) {
+    background(200, 0, 100);
   }
+
   bubble1.show();
   bubble2.show();
   bubble1.move();
-  bubble2.move();
+  // bubble2.move();
+  bubble2.x = mouseX;
+  bubble2.y = mouseY;
 }
 
 class Bubble {
-  constructor(x, y, r=50) {
+  constructor(x, y, r = 50) {
     this.x = x;
     this.y = y;
     this.r = r;
     this.brightness = 0;
   }
 
-  changeColor(bright){
-this.brightness = bright;
+  intersects(other) {
+    let d = dist(this.x, this.y, other.x, other.y);
+    return (d < this.r + other.r);
+    // if (d < this.r + other.r) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
   }
 
-  contains(px, py){
+  changeColor(bright) {
+    this.brightness = bright;
+  }
+
+  contains(px, py) {
     let d = dist(px, py, this.x, this.y);
-    if(d < this.r){
+    if (d < this.r) {
       return true;
-    } else{
+    } else {
       return false;
     }
   }
-  
+
   move() {
     this.x = this.x + random(-2, 2);
     this.y = this.y + random(-2, 2);
